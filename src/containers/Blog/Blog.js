85aae6818d51e56f4a06f6a29900fc16eddae70c
akshpnwr <akashpanwar9976@gmail.com>
2021-03-07 21:03:25 +0530

    fetching and displaying data using axios

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,25 +4,58 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import axios from 'axios';
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  state = {
+    posts: [],
+    selectPostId: null,
+  };
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts').then((res) => {
+      const posts = res.data.slice(0, 4).map((post) => {
+        return {
+          ...post,
+          author: 'aksh',
+        };
+      });
+      this.setState({
+        posts: posts,
+      });
+    });
+  }
+
+  postsHandler = (id) => {
+    this.setState({
+      selectPostId: id,
+    });
+  };
+
+  render() {
+    const posts = this.state.posts.map((post) => {
+      return (
+        <Post
+          key={post.id}
+          title={post.title.slice(0, 11)}
+          author={post.author}
+          clicked={() => this.postsHandler(post.id)}
+        />
+      );
+    });
+
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <section>
+          <FullPost id={this.state.selectPostId} />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
